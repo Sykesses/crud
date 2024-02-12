0fb3ad3b47d0690ecd6cd4c5bc25f8e146fcbb2d
Ivan Mikitko <sykesses@gmail.com>
2024-02-13 02:52:07 +0300

    feat: cluster.js

---
+++ cluster.js
@@ -0,0 +1,26 @@
+const cluster = require("cluster");
+const os = require("os");
+
+if (cluster.isMaster) {
+  const numWorkers = os.cpus().length - 1;
+
+  console.log(`Master cluster setting up ${numWorkers} workers...`);
+
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("online", (worker) => {
+    console.log(`Worker ${worker.process.pid} is online`);
+  });
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died with code: ${code} and signal: ${signal}`
+    );
+    console.log("Starting a new worker");
+    cluster.fork();
+  });
+} else {
+  require("./server.js");
+}
